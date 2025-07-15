8cff3a45be92a676fa92209f346e55a61df079c8
coder-0626 <milindnipurte6662@gmail.com>
2025-07-15 20:11:31 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,27 @@
+package com.eclip;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		int age = 22;
+		String name = "milind";
+		double val = 12.22;
+		
+		System.out.println(age);
+		System.out.println(name);
+		System.out.println(val);
+		System.out.println(name+age);
+
+	}
+
+}
+	class Quize11{
+		public static void main(String[] args) {
+			int a= 10;
+			int b = 5;
+			int cal = (a * b) / (a-b);
+			//int cal = a * b / a-b ;
+			System.out.println(cal);
+		}
+
+	}
